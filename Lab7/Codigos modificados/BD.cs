d94fc62a28942aa9b11db2c8f61ce477d8428869
chuancao26 <109006392+chuancao26@users.noreply.github.com>
2023-07-20 10:24:07 -0500

    Agregando BD class

--- Lab7/Codigos modificados/BD.cs
+++ Lab7/Codigos modificados/BD.cs
@@ -1,15 +1,71 @@
-        private void createSesion(String Nombre, String Apellido)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Contracts;
+
+namespace Base_Datos.DAO
+{
+    public class BD
+    {
+        private string connectionString;
+        private SqlConnection con;
+        public void InsertAlumno(string nom,string ape,string sex,string ema, string dir,int ciu,string req)
         {
-            Session["Nombre"] = Nombre;
-            Session["Apellido"] = Apellido;
+            connectionString = "Data Source=(localdb)\\ProjectModels;Initial Catalog=DBP;Integrated Security=True";
+            con = new SqlConnection(connectionString);
+            con.Open();
+            string query = "INSERT INTO DataAlumnos VALUES (@Nom,@Ape,@Sex,@Ema,@Dir,@Ciu,@Req)";
+            SqlCommand command = new SqlCommand(query, con);
+            command.Parameters.AddWithValue("@Nom", nom);
+            command.Parameters.AddWithValue("@Ape", ape);
+            command.Parameters.AddWithValue("@Sex", sex);
+            command.Parameters.AddWithValue("@Ema", ema);
+            command.Parameters.AddWithValue("@Dir", dir);
+            command.Parameters.AddWithValue("@Ciu", ciu);
+            command.Parameters.AddWithValue("@Req", req);
+            command.ExecuteNonQuery();
+            con.Close();
         }
 
-        //creacion de las cookies
-        private void createCookie(String Sexo, String Ciudad)
+        public List<string> SelectCiudad()
         {
-            HttpCookie cookie1 = new HttpCookie("sexo", Sexo);
-            HttpCookie cookie2 = new HttpCookie("ciudad", Ciudad);
-            Response.Cookies.Add(cookie1);
-            Response.Cookies.Add(cookie2);
-            
+            List<string> ciudades = new List<string>();
+            connectionString = "Data Source=(localdb)\\ProjectModels;Initial Catalog=DBP;Integrated Security=True";
+            con = new SqlConnection(connectionString);
+            con.Open();
+            string query = "SELECT Ciudad FROM DataCiudad";
+            SqlCommand command = new SqlCommand(query, con);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string ciu=reader["Ciudad"].ToString();
+                ciudades.Add(ciu); 
+            }
+            con.Close();
+            return ciudades;
         }
+        public int SelectIdCiudad(string ciudad)
+        {
+            string id="0";
+            connectionString = "Data Source=(localdb)\\ProjectModels;Initial Catalog=DBP;Integrated Security=True";
+            con = new SqlConnection(connectionString);
+            con.Open();
+            string query = "SELECT Id FROM DataCiudad WHERE Ciudad=@Ciudad";
+            SqlCommand command = new SqlCommand(query, con);
+            command.Parameters.AddWithValue("@Ciudad", ciudad);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                id = reader["Id"].ToString();
+            }
+            con.Close();
+            return int.Parse(id);
+        }
+    }
+}
