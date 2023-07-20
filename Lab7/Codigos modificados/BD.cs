0cbe995519a2d1aef09f2e71964563df3ed0d4d9
chuancao26 <109006392+chuancao26@users.noreply.github.com>
2023-07-20 11:01:36 -0500

    Agregando BD class

--- Lab7/Codigos modificados/BD.cs
+++ Lab7/Codigos modificados/BD.cs
@@ -7,64 +7,62 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics.Contracts;
 
-namespace Base_Datos.DAO
+namespace ProjectDataBase.DataBaseCode
 {
     public class BD
     {
         private string connectionString;
-        private SqlConnection con;
-        public void InsertAlumno(string nom,string ape,string sex,string ema, string dir,int ciu,string req)
-        {
-            connectionString = "Data Source=(localdb)\\ProjectModels;Initial Catalog=DBP;Integrated Security=True";
-            con = new SqlConnection(connectionString);
-            con.Open();
-            string query = "INSERT INTO DataAlumnos VALUES (@Nom,@Ape,@Sex,@Ema,@Dir,@Ciu,@Req)";
-            SqlCommand command = new SqlCommand(query, con);
-            command.Parameters.AddWithValue("@Nom", nom);
-            command.Parameters.AddWithValue("@Ape", ape);
-            command.Parameters.AddWithValue("@Sex", sex);
-            command.Parameters.AddWithValue("@Ema", ema);
-            command.Parameters.AddWithValue("@Dir", dir);
-            command.Parameters.AddWithValue("@Ciu", ciu);
-            command.Parameters.AddWithValue("@Req", req);
-            command.ExecuteNonQuery();
-            con.Close();
-        }
-
-        public List<string> SelectCiudad()
+        private SqlConnection connection;
+        public List<string> getCiudades()
         {
             List<string> ciudades = new List<string>();
-            connectionString = "Data Source=(localdb)\\ProjectModels;Initial Catalog=DBP;Integrated Security=True";
-            con = new SqlConnection(connectionString);
-            con.Open();
+            connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = WEBDB1; Integrated Security = True";
+            connection = new SqlConnection(connectionString);
+            connection.Open();
             string query = "SELECT Ciudad FROM DataCiudad";
-            SqlCommand command = new SqlCommand(query, con);
+            SqlCommand command = new SqlCommand(query, connection);
             SqlDataReader reader = command.ExecuteReader();
-
             while (reader.Read())
             {
-                string ciu=reader["Ciudad"].ToString();
-                ciudades.Add(ciu); 
+                string city = reader["Ciudad"].ToString();
+                ciudades.Add(city);
             }
-            con.Close();
+            connection.Close();
             return ciudades;
         }
-        public int SelectIdCiudad(string ciudad)
+        public void insertarRecord(string nom, string ape, string sex, string ema, string dir, int ciu, string req)
         {
-            string id="0";
-            connectionString = "Data Source=(localdb)\\ProjectModels;Initial Catalog=DBP;Integrated Security=True";
-            con = new SqlConnection(connectionString);
-            con.Open();
+            connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = WEBDB1; Integrated Security = True";
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            string query = "INSERT INTO [dbo].[DataAlumnos] ([Nombre], [Apellidos], [Sexo] ,[Email] , [Direccion], [CodeCiudad], [Requerimiento])" +
+                "VALUES (@Nom,@Ape,@Sex, @Ema,@Dir,@Ciu,@Req)";
+            SqlCommand sqlcommand = new SqlCommand(query, connection);
+            sqlcommand.Parameters.AddWithValue("@Nom", nom);
+            sqlcommand.Parameters.AddWithValue("@Ape", ape);
+            sqlcommand.Parameters.AddWithValue("@Sex", sex);
+            sqlcommand.Parameters.AddWithValue("@Ema", ema);
+            sqlcommand.Parameters.AddWithValue("@Dir", dir);
+            sqlcommand.Parameters.AddWithValue("@Ciu", ciu);
+            sqlcommand.Parameters.AddWithValue("@Req", req);
+            sqlcommand.ExecuteNonQuery();
+            connection.Close();
+        }
+        public int getIdFromCity(string ciudad)
+        {
+            string id = "0";
+            connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = WEBDB1; Integrated Security = True";
+            connection = new SqlConnection(connectionString);
+            connection.Open();
             string query = "SELECT Id FROM DataCiudad WHERE Ciudad=@Ciudad";
-            SqlCommand command = new SqlCommand(query, con);
+            SqlCommand command = new SqlCommand(query, connection);
             command.Parameters.AddWithValue("@Ciudad", ciudad);
             SqlDataReader reader = command.ExecuteReader();
-
             while (reader.Read())
             {
                 id = reader["Id"].ToString();
             }
-            con.Close();
+            connection.Close();
             return int.Parse(id);
         }
     }
